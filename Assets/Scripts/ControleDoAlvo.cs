e02b47e659ec3a006ca32e97626f61464b2e3886
zemarcelo <>
2019-07-17 19:02:37 -0300

    script de controle do alvo

--- Assets/Scripts/ControleDoAlvo.cs
+++ Assets/Scripts/ControleDoAlvo.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class ControleDoAlvo : MonoBehaviour
 {
 
+    public float velocidade = 10.0f;
+    public float velocidadeDeRotacao = 100.0f;
+
     private Transform transformDoAlvo;
 
     // Start is called before the first frame update
@@ -16,6 +19,12 @@ public class ControleDoAlvo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        float translacao = Input.GetAxis("Vertical") * velocidade * Time.deltaTime;
+        float rotacao = Input.GetAxis("Horizontal") * velocidadeDeRotacao * Time.deltaTime;
+
+        transformDoAlvo.Translate(0, 0, translacao);
+        transformDoAlvo.Rotate(0, rotacao, 0);
+
     }
 }
