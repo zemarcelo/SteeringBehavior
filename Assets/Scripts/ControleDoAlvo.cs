077a7be2fac68a4f6d090ffae941a2ef95c1a32c
zemarcelo <zemarcelo@cafepuro.net.br>
2019-07-17 12:41:08 -0300

    Meu primeiro commit!

---
+++ Assets/Scripts/ControleDoAlvo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleDoAlvo : MonoBehaviour
+{
+
+    private Transform transformDoAlvo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transformDoAlvo = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
